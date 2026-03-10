4e6a7ec614becddd01d758f99634623d6c1df5dc
Davidan1005 <154266705+Davidan1005@users.noreply.github.com>
2026-03-10 19:59:35 +0100

    Implemented a system for simulating a single link of fixed length

--- single_link.py
+++ single_link.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Initialisation of static shit
-fig, ax = plt.subplots()
+# Initialisation of static stuff
+fig, ax = plt.subplots(figsize = (7,7))
 
 ax.set_xlim(-10,10)
 ax.set_ylim(-10,10)
@@ -11,27 +11,30 @@ originx = 0
 originy = 0
 
 ax.plot([originx],[originy],'o')
-# Initialisation of static shit
+# Initialisation of static stuff
 
 
-# Initialisation of non static shit
-dot, = ax.plot([0], [0], "ro")
+# Initialisation of non static stuff
+cursor_dot, = ax.plot([0], [0], "ro")
 line, = ax.plot([originx,0],[originy,0])
 link_end, = ax.plot([0], [0], "o")
-# Initialisation of non static shit
+# Initialisation of non static stuff
+
+
 
 
 def track_cursor(event):
+    """Tracks cursor movement and updates variables that depend on new state"""
     if event.inaxes:
-        dot.set_data([event.xdata], [event.ydata])
+        cursor_dot.set_data([event.xdata], [event.ydata])
 
-        clamping_constant = ((event.xdata**2) + (event.ydata**2))**0.5
+        clamping_constant = np.hypot(event.xdata,event.ydata)
         length_constant = 5
 
         line.set_data([originx,(event.xdata*length_constant)/clamping_constant], [originy,event.ydata*length_constant/clamping_constant])
-        
         link_end.set_data([event.xdata*length_constant/clamping_constant], [event.ydata*length_constant/clamping_constant])
-        # line.set_data([originx,event.xdata], [originy,event.ydata])
+
+
         
         fig.canvas.draw_idle()
 
