8eafa567b86a3cab952e23a894ccf88766c1752b
Davidan1005 <154266705+Davidan1005@users.noreply.github.com>
2026-03-11 15:27:40 +0100

    seperated methods for forward kinematics, draw and init. Everything still works

--- double_link.py
+++ double_link.py
@@ -11,31 +11,39 @@ class Link():
 
         
     def get_components(self,absolute_angle):
-        self.components = np.array([
+        return np.array([
             (self.length*np.cos(absolute_angle)),
             (self.length*np.sin(absolute_angle))
         ])
-        return self.components
+        
 
 
 
 
 class Arm():
-    def __init__(self, links):
 
+    def __init__(self, links):
         self.links = links
+
+    def forward_kinematics(self):
         cumulative_position = np.array([0, 0])
         cumulative_angle = 0
 
+        joint_positions = [cumulative_position.copy()]
+
         for link in self.links:
             cumulative_angle += link.angle
+            cumulative_position = cumulative_position + link.get_components(cumulative_angle)
 
-            ax.plot([cumulative_position[0], cumulative_position[0]+link.get_components(cumulative_angle)[0]],
-                    [cumulative_position[1], cumulative_position[1]+link.get_components(cumulative_angle)[1]], marker='o')
+            joint_positions.append(cumulative_position.copy())
 
-            cumulative_position = cumulative_position+link.components
+    
+
+        return np.array(joint_positions) 
+
+    def draw(self,ax, coordinates):
+        ax.plot(coordinates[:,0],coordinates[:,1], marker='o')
             
-
 
 # Static stuff (Initialises plotting space and shit)
 fig, ax = plt.subplots()
@@ -45,12 +53,14 @@ ax.set_xlim(-10, 10)
 ax.set_ylim(-10, 10)
 # Static stuff
 
-link1 = Link(np.pi/2)
+link1 = Link(np.pi/6)
 link2 = Link(np.pi/2)
 
 links = [link1, link2]
 
 arm = Arm(links)
+coordinates = arm.forward_kinematics()
+arm.draw(ax,coordinates)
 
 
 plt.show()
